2210064e0bb36d7c70c9df97d570406e5b323ebe
Yehonatan <yehonatan.goldfarb@gmail.com>
2022-02-20 13:42:14 +0200

    first commit

---
+++ Models.py
@@ -0,0 +1,63 @@
+# used https://github.com/claudeHifly/BipedalWalker-v3/blob/master as template
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Actor(nn.Module):
+    """
+    Actor network maps states to an action
+    """
+    def __init__(self, state_size, action_size, seed):
+        """
+        Build a fully connected neural network
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, 600)
+        self.fc2 = nn.Linear(600, 300)
+        self.fc3 = nn.Linear(300, action_size)
+
+        self.bn1 = nn.BatchNorm1d(600)
+        self.bn2 = nn.BatchNorm1d(300)
+
+
+        self.fc2.weight.data.uniform_(-1.5e-3, 1.5e-3)
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = self.fc3(x)
+
+        return F.tanh(x)
+
+
+class Critic(nn.Module):
+    """
+    Critic network maps (state, action) pair to its value
+    """
+    def __init__(self, state_size, action_size, seed):
+        """
+        Build a fully connected neural network
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, 600)
+        self.fc2 = nn.Linear(600, 300)
+        self.fc3 = nn.Linear(300, 1)
+
+        self.action_fc = nn.Linear(action_size, 1)
+
+        self.bn1 = nn.BatchNorm1d(600)
+
+        self.fc2.weight.data.uniform_(-1.5e-3, 1.5e-3)
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, x_state, action):
+        x_state = F.relu(self.bn1(self.fc1(x_state)))
+        x_state = self.fc2(x_state)
+        x_action = self.action_fc(action)
+        x = F.relu(torch.ad(x_state, x_action))
+        return self.fc3(x)
+
---
+++ ddpg.py
@@ -0,0 +1,89 @@
+import torch
+from Models import *
+import Memory
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class DDPGAgent:
+    def __init__(self, state_size, action_size, seed):
+        """
+        :param state_size: number of variables in each state
+        :param action_size: number of variables in each action
+        :param seed:
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.timestep = 0
+        self.batch_size = 64
+        self.buffer_size = 1000000
+        self.actor_lr = 0.0001
+        self.critic_lr = 0.001
+        self.gamma = .99
+        self.tau = 0.001
+        self.epsilon = 1.0
+        self.epsilon_min = .01
+        self.epsilon_decay = .996
+        self.should_be_updated = 50
+
+        # ---------networks initialization--------#
+        # -----actor network-----#
+        self.acotr = Actor(state_size, action_size, seed).to(device)
+        self.actor_optimizer = optim.Adam(self.acotr.parameters(), lr=self.actor_lr)
+
+        # -----critic networks-----#
+        self.critic_online = Critic(state_size, action_size, seed).to(device)
+        self.critic_target = Critic(state_size, action_size, seed).to(device)
+        self.critic_optimizer = optim.Adam(self.critic_online.parameters(), lr=self.critic_lr)
+
+        # -----initialize memory-----#
+        self.memory = Memory(buffer_size=self.buffer_size, batch_size=self.batch_size, seed=seed)
+
+    def step(self, state, action, reward, next_state, done):
+        self.timestep += 1
+        # add current experience to the memory
+        self.memory.add(state, action, reward, next_state, done)
+        # if there's enough experience samples in the memory - learn
+        if len(self.memory) > self.batch_size:
+            sampled_experinces = self.memory.sample()
+            self.learn(sampled_experinces)
+
+    def learn(self, experiences):
+        states, actions, rewards, next_states, dones = experiences
+
+        # ----update actor----#
+        predicted_actions = self.acotr(states)
+        actor_loss = -self.critic_online(states, predicted_actions).mean()
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # ----update critic----#
+        next_pred_actions = self.acotr(next_states)
+        next_Q_targets = self.critic_target(next_states, next_pred_actions)
+        curr_Q_targets = rewards + (self.gamma * next_Q_targets * (1-dones))
+        # compute loss
+        curr_Q_predicted = self.critic_online(states, actions)
+        critic_loss = F.mse_loss(curr_Q_predicted, curr_Q_targets)
+        # step
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # update target network
+        if self.timestep % self.should_be_updated == 0:
+            for target_param, local_param in zip(self.critic_target.parameters(), self.critic_online.parameters()):
+                target_param.data.copy_(local_param.data)
+
+
+    def act(self, state):
+        # TODO: epsilon greedy
+        # TODO: add noise?
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.acotr(state).cpu().data.numpy()
+        self.actor.train()
+        return action
+
